9f047536dc82fdaf454d953478af4628191a2482
andrew-atef <49458507+andrew-atef@users.noreply.github.com>
2020-07-04 19:41:32 +0200

    1

---
+++ precache-manifest.1ffa8d3b3d39dde912a760a8a961c6ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083014e763f63c59eae5",
+    "url": "/css/app.80afd824.css"
+  },
+  {
+    "revision": "c27a81fcd666a790497e",
+    "url": "/css/chunk-vendors.0ba3e87d.css"
+  },
+  {
+    "revision": "ab14e31ac544715f799aff4b050ab28d",
+    "url": "/img/kisspng-earth-planet-icon-space-planet-png-hd-5a7844ad9b0f15.2487239715178313416351.ab14e31a.png"
+  },
+  {
+    "revision": "360ed6eac5e85403b504cb4aab6b8af9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "083014e763f63c59eae5",
+    "url": "/js/app.9b82d6b8.js"
+  },
+  {
+    "revision": "c27a81fcd666a790497e",
+    "url": "/js/chunk-vendors.29622441.js"
+  },
+  {
+    "revision": "2d7c4ff42689a28a4591a22577f75e14",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
